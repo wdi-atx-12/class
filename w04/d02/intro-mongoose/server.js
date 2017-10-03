4611194604e89b1b4ee3ccb360d0f43ebf9e6db4
Britney Jo <me@britneyjo.com>
2017-10-03 16:25:35 -0500

    add routes with database functionality

--- w04/d02/intro-mongoose/server.js
+++ w04/d02/intro-mongoose/server.js
@@ -15,8 +15,65 @@ var User = require('./models/user');
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
 
 // app routes
+// get all users
+app.get('/users', function(req, res){
+  User.find({}, function(err, allUsers){
+    if (err) throw err;
+    res.json({ users: allUsers })
+  });
+});
+
+// create new user
+app.post('/users', function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var dob = req.body.dob;
+  var strengths = req.body.strengths;
+  var vegetarian = req.body.vegetarian;
+
+  var newUser = User({
+    firstName: firstName,
+    lastName: lastName,
+    dob: dob,
+    strengths: strengths,
+    vegetarian: vegetarian
+  });
+
+  newUser.save(function(err, user){
+    res.send(`user created: ${user}`)
+  });
+});
+
+// get one user
+app.get('/users/:id', function(req, res){
+  var userId = req.params.id;
+  User.findOne({_id: userId}, function(err, foundUser){
+    res.json(foundUser)
+  })
+});
+
+// update one user
+app.put('/users/:id', function(req, res){
+  var userId = req.params.id;
+  var updatedUser = {
+    firstName: req.body.firstName
+  }
+  User.findByIdAndUpdate(userId, updatedUser, {new: true}, function(err, modifiedUser){
+    res.json(modifiedUser);
+  })
+});
+
+// delete one user
+app.delete('/users/:id', function(req, res){
+  var userId = req.params.id;
+  User.findOneAndRemove({_id: userId}, function(err, deletedUser){
+    res.json(deletedUser);
+  });
+});
+
 
 // app start
 app.listen(3000, function(){

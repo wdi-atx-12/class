ecbd34e64818abb42177e152d6144ef25ca8f911
Britney Jo <me@britneyjo.com>
2017-10-11 14:46:35 -0500

    created student directory and set up express server

---
+++ w05/d03/student-directory/server.js
@@ -0,0 +1,23 @@
+// dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// app set up
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// app routes
+app.get('/', function(req,res){
+  res.send("Hi there!")
+});
+
+// app start
+app.listen(3000, function(){
+  console.log('Server running on port 3000')
+});
